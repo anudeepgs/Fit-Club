3f4a1ca07b9f3ee6090b85d6b2a9e07a136b93c5
Anudeep G S <anudeepgs31@gmail.com>
2022-09-06 12:50:27 +0530

    leftside update commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,6 +12,30 @@ const Hero = () => {
       <div></div>
       <span>The Best Fittness Club</span>
     </div>
+
+    <div className="hero-text">
+      <div>
+        <span className='stroke-text'>Shape  </span>
+        <span>Your</span>
+      </div>
+      <div><span>Ideal Body</span></div>
+      <div><span>In here we will help you to shape and build your ideal body and live up your life to fullest</span></div>
+    </div>
+    {/*figuers*/}
+    <div className="figures">
+      <div>
+        <span>140+</span>
+        <span>expert coachs</span>
+      </div>
+      <div>
+        <span>150+</span>
+        <span>Members joined</span>
+      </div>
+      <div>
+        <span>50+</span>
+        <span>Fitness Programs</span>
+      </div>
+    </div>
 </div>
     <div className="right-h"></div>
    </div>
